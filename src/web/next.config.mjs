acef1b561c1f32df56db53bd98dcdb98e83fa52d
AlexHang <alexandruhang@yahoo.com>
2025-03-15 15:30:13 +0200

    fix: fix export for FE deployment

--- src/web/next.config.mjs
+++ src/web/next.config.mjs
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   images: {
     domains: ['upload.wikimedia.org', 'images-assets.nasa.gov', 'science.nasa.gov'],
   },
